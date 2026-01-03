54a3a6eb986850a8e1475a75aa7c38a4c1462c68
Ricardo Francisco Neves Estêvão <ricardorebuild15@gmail.com>
2026-01-03 15:56:06 +0000

    Refactor: Removido código desnecessário e melhorada a configuração da conexão com o banco de dados

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root123', // Define no .env ou aqui
-    database: process.env.DB_NAME || 'quero_emigrar_db'
+    database: process.env.DB_NAME || 'migracao'
 });
 
 connection.connect(error => {
